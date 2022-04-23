76748a34a5c0a0627e5063118cb94198e7488290
Jão Eduardo <90727738+sapoiaultra@users.noreply.github.com>
2022-04-23 15:18:18 +0000

    Foi adicionada uma nova linha no array.

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ let nomes = ["João", "Ana", "Carlos"];
 let outros_nomes = nomes; // cópia por referência
 
 outros_nomes[1] = "Antônio";
+console.log(outros_nomes, nomes);
+
+// os indices podem não ser sequênciais
+nomes[100]= "Joaquim";
 console.log(nomes);
+
+// importante!
+// push e pop são mais rápidos que shift e unshift que vimos anteriormente.
