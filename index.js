eb7d438ed2089424da1240122483f5ebb0a8f8b1
Jão Eduardo <90727738+sapoiaultra@users.noreply.github.com>
2022-04-23 15:38:10 +0000

    Foram adicionados mais códigos de impressão.

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ console.log(nomes);
 
 // importante!
 // push e pop são mais rápidos que shift e unshift que vimos anteriormente.
+// existe uma outra forma de iteração nos arrays
+for(let nome of nomes){ // OF extrai um nome a cada ciclo
+  console.log(nome)
+}
+
+// ou (não aconselhavel por razões técnicas)
+for(let k in nomes){ // IN se refere explicitamente as chaves 
+  console.log(nomes[k]);
+}
+
+// length pode não corresponder ao número total de elementos do array.
+// length não conta o numero de elementos. Vê apenas o último indice e acrescenta + 1.
+
+console.log(nomes.length);
